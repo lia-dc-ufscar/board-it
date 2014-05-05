0d8b15b6f6bc15af596b6a906fc8369bc6d66963
mrtakata <matheustakata@dhcp-128-189-68-10.ubcsecure.wireless.ubc.ca>
2014-05-05 16:24:50 -0700

    initial commit

--- postits.js
+++ postits.js
@@ -1,15 +1,8 @@
-Posts = new Meteor.Collection("posts");
-
 if (Meteor.isClient) {
   Meteor.startup(function(){
-    Session.set("current_page","home");
+    Session.set("current_page","teste");
   }),
-  Template.post_list.posts = function(){
-    return Posts.find({}, {sort: {date: -1}});
-  },
-  Template.screen.posts = function(){
-    return Posts.find({}, {sort: {date: -1}});
-  },
+
   Template.page.is_list = function(){
     return Session.equals("current_page", "list");
   },
@@ -18,6 +11,12 @@ if (Meteor.isClient) {
   },
   Template.page.is_home = function(){
     return Session.equals("current_page", "home");
+  },
+  Template.page.is_new_image = function(){
+    return Session.equals("current_page", "new_img");
+  },
+  Template.page.is_teste = function(){
+    return Session.equals("current_page", "teste");
   },
 
   Template.page.events = {
@@ -35,56 +34,23 @@ if (Meteor.isClient) {
 
       Session.set("current_page", "new");
       console.log(Session.get("current_page"));
-    }
-  },
-
-  Template.new_post.preview = function(){
-      author = $('textarea[name=author]').val().split("\n").join("<br/>");  
-      content = $('textarea[name=content]').val().split("\n").join("<br/>");
-      bgColor = document.getElementById("bColor").value;
-      fontColor = document.getElementById("fColor").value;
-      size = document.getElementById("size").value;
-  },
-  Template.new_post.events ={
-    'click button.submit': function(){
-      author = $('textarea[name=author]').val().split("\n").join("<br/>");  
-      content = $('textarea[name=content]').val().split("\n").join("<br/>");
-      bgColor = document.getElementById("bColor").value;
-      fontColor = document.getElementById("fColor").value;
-      size = document.getElementById("size").value;
-      posTop = document.getElementById("posTop").value;
-      posLeft = document.getElementById("posLeft").value;
-
-      if ( author == ""){
-        author = "Anonymous";
-      }
-      if ( content != "" ){
-        Posts.insert ({author: author, content: content, bgColor: bgColor, fColor: fontColor, size: size, posTop: posTop, posLeft: posLeft,date: new Date});
-        alert("Post created!");
-        Session.set("current_page", "home");
-      }
-      else{
-        alert("Digite o conteúdo do post");
-      }
-    }
-  },
-  Template.post_info.events = {
-    'click .btn-danger': function(){
-      Posts.remove(this._id);
+    },
+    'click a#newImg': function(){
+      Session.set("current_page", "new_img");
+      console.log(Session.get("current_page"));
+    },
+    'click a#teste': function(){
+      Session.set("current_page", "teste");
+      console.log(Session.get("current_page"));
     }
   }
-  /*
-  Template.post.rendered = function(){
-    console.log('rendered');
-    $('.draggable').length();
-  }
-  */
+
 }
 
 if(Meteor.isServer){
   Meteor.startup(function(){
     if (Posts.find().count() === 0){
-      Posts.insert ({author: "Me", content: "First!", bgColor: "blue", fColor: "white", size: "3x3", posTop: "1", posLeft:"1", date: new Date});
+      Posts.insert ({author: "Me", content: "First!", bgColor: "blue", fColor: "white", size: "3x3", posTop: 5, posLeft: 5, date: new Date});
     }
   });
 }
